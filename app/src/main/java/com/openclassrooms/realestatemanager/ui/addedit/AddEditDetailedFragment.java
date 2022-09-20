5118131b2ad690269340f904c0be583ba0d9f6e6
Anne <anne-linger@orange.fr>
2022-09-20 11:24:31 +0200

    Navigation components works for add/edit detailed fragment

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/addedit/AddEditDetailedFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/addedit/AddEditDetailedFragment.java
@@ -12,10 +12,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.databinding.FragmentDetailedDataBinding;
 import com.openclassrooms.realestatemanager.ui.main.MainActivity;
+import com.openclassrooms.realestatemanager.viewmodels.AddViewModel;
 
 /**
 *Fragment to add or edit detailed information about property
@@ -26,6 +32,12 @@ public class AddEditDetailedFragment extends Fragment {
     //For ui
     private FragmentDetailedDataBinding mBinding;
 
+    //For navigation
+    private NavController mNavController;
+
+    //For data
+    private AddViewModel mAddViewModel;
+
     public static AddEditDetailedFragment newInstance() {
         return new AddEditDetailedFragment();
     }
@@ -34,24 +46,63 @@ public class AddEditDetailedFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.e("", "onCreateFragmentDetailed");
         mBinding = FragmentDetailedDataBinding.inflate(inflater, container, false);
-        setHasOptionsMenu(true);
         return mBinding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        this.mNavController = Navigation.findNavController(view);
         configureToolbar();
+        configureBottomNav();
+        configureViewModel();
+        goToNextFragmentAddEdit();
     }
 
-    //TODO create a alert dialog to confirm cancel add/edit
     private void configureToolbar() {
         Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
-        toolbar.setNavigationOnClickListener(view -> navigateToMainActivity());
+        toolbar.setTitle(this.getString(R.string.add_edit_detailed_title));
+        toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+        toolbar.setNavigationOnClickListener(view -> showDialogToConfirmCancel());
+    }
+
+    private void configureBottomNav() {
+        BottomNavigationView bottomNavigationView = getActivity().findViewById(R.id.bottom_nav);
+        bottomNavigationView.setVisibility(View.GONE);
+    }
+
+    //TODO attacher le VM au fragment
+    private void configureViewModel() {
+        mAddViewModel = new ViewModelProvider(requireActivity()).get(AddViewModel.class);
+    }
+
+    //TODO create an utils for that !
+    //Dialog to alert about the add/edit annulment
+    public void showDialogToConfirmCancel() {
+        MaterialAlertDialogBuilder alertDialogBuilder = new MaterialAlertDialogBuilder((this.requireContext()), R.style.AlertDialogTheme);
+        alertDialogBuilder.setTitle(R.string.cancel_add_edit_title)
+                .setMessage(R.string.confirm_cancel_message)
+                .setCancelable(false)
+                .setPositiveButton(R.string.cancel_button, (dialog, which) -> {
+                    navigateToMainActivity();
+                })
+                .setNegativeButton(R.string.continue_button, (dialog, which) -> dialog.dismiss())
+                .create()
+                .show();
+    }
+
+    private void goToNextFragmentAddEdit() {
+        mBinding.btSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("", "onClick");
+                mAddViewModel.createProperty("House", 120000, 80, 5, null, "12 Main Street", "New York", true, "13 octobre 2022", null, "Bob Stuart");
+                navigateToMainActivity();
+            }
+        });
     }
 
     private void navigateToMainActivity() {
-        Intent intent = new Intent(requireActivity(), MainActivity.class);
-        startActivity(intent);
+        mNavController.navigate(R.id.action_addEditDetailedFragment_to_listViewFragment);
     }
 }
--- app/src/main/java/com/openclassrooms/realestatemanager/ui/addedit/AddEditGeneralFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/addedit/AddEditGeneralFragment.java
@@ -1,20 +1,11 @@
 package com.openclassrooms.realestatemanager.ui.addedit;
 
-import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Rect;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,8 +21,6 @@ import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.databinding.FragmentGeneralDataBinding;
 import com.openclassrooms.realestatemanager.ui.main.MainActivity;
 import com.openclassrooms.realestatemanager.viewmodels.AddViewModel;
-
-import java.util.Objects;
 
 /**
 *Fragment to add or edit general information about property
@@ -66,7 +55,7 @@ public class AddEditGeneralFragment extends Fragment {
         configureToolbar();
         configureBottomNav();
         configureViewModel();
-        saveProperty();
+        goToNextFragmentAddEdit();
     }
 
     private void configureToolbar() {
@@ -93,39 +82,20 @@ public class AddEditGeneralFragment extends Fragment {
                 .setMessage(R.string.confirm_cancel_message)
                 .setCancelable(false)
                 .setPositiveButton(R.string.cancel_button, (dialog, which) -> {
-                    navigateToMainActivity();
+                    mNavController.navigate(R.id.action_addEditGeneralFragment_to_listViewFragment);
                 })
                 .setNegativeButton(R.string.continue_button, (dialog, which) -> dialog.dismiss())
                 .create()
                 .show();
     }
 
-    //TO delete only for test during dev
-    private void saveProperty() {
+    private void goToNextFragmentAddEdit() {
         mBinding.btNextAddEdit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.e("", "onClick");
-                mAddViewModel.createProperty("House", 120000, 80, 5, null, "12 Main Street", "New York", true, "13 octobre 2022", null, "Bob Stuart");
-                //mNavController.navigate(R.id.action_addEditGeneralFragment_to_addEditDetailedFragment);
-                //navigateToMainActivity();
+                mNavController.navigate(R.id.action_addEditGeneralFragment_to_addEditDetailedFragment);
             }
         });
-    }
-
-    //TODO Like in the adapter, renvoi  de la nav vers la main activity pour aller vers fagment addEditDetailed
-    private void navigateToAddEditFragment() {
-        /**if (!mAddEditDetailedFragment.isVisible()) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.activity_places_frame_layout, mAddEditDetailedFragment).commit();
-        }
-        mBinding.bottomNav.setVisibility(View.GONE);
-        mBinding.mainActivityLayout.closeDrawers();
-        mBinding.toolbar.setTitle(this.getString(R.string.add_edit_general_title));
-        mBinding.toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);*/
-    }
-
-    private void navigateToMainActivity() {
-        Intent intent = new Intent(requireActivity(), MainActivity.class);
-        startActivity(intent);
     }
 }
