67a9431a013378a882012c0ca35c192ef1898643
Anne <anne-linger@orange.fr>
2022-09-25 09:27:48 +0200

    Display a Google Map in map fragment

--- app/src/main/java/com/openclassrooms/realestatemanager/ui/mapview/MapViewFragment.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/ui/mapview/MapViewFragment.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.realestatemanager.ui.mapview;
 
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,16 +12,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.openclassrooms.realestatemanager.R;
 import com.openclassrooms.realestatemanager.databinding.FragmentMapViewBinding;
 
 /**
 *Created by Anne Linger on 14/09/2022.
 */
 
-public class MapViewFragment extends Fragment {
+public class MapViewFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
     //For UI
     private FragmentMapViewBinding mBinding;
+    private GoogleMap mGoogleMap;
+    private float zoom = 12;
 
     public static MapViewFragment newInstance() {
         return new MapViewFragment();
@@ -33,5 +45,34 @@ public class MapViewFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        assert supportMapFragment != null;
+        supportMapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        updateMapLocation();
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+    }
+
+    //Update map with user location
+    private void updateMapLocation() {
+        //mLocation = location;
+        //mLocationString = mLocation.getLatitude() + "," + mLocation.getLongitude();
+        //mPlacesViewModel.fetchNearbySearchPlaces(mLocationString);
+        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        Activity activity = getActivity();
+        /**if (isAdded() && activity != null) {
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                    .title(requireActivity().getString(Integer.parseInt(getString(R.string.my_position)))));
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), zoom));*/
+
     }
 }
